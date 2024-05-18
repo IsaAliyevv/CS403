a5e80d5ce341d0696f2143e77c8efc8a9623324c
isa aliyev <isaaliyevv888@gmail.com>
2024-05-18 20:32:25 +0400

    feat: introduction

---
+++ 02.Non_Primitive/script.js
@@ -0,0 +1,24 @@
+//string,number,bigint,Boolean,Symbol,null,undefined-primitive
+//Object öz daxilində bölünür
+// Array.Object.Error.Date.etc
+
+//const usr ={
+    //firstName:" Isa",
+    //lastName:"Aliyev",
+    ///age:"18",
+    //info:{
+       // adress:"Baku",
+       // city:"Baku",
+       // country:"Baku",
+    //},
+    //edu: {
+       // uni:"none",
+        //point:"0",
+    //},
+    //work: {
+            //company:"ema mmc",
+            //exper:"4",
+       // }
+    //}
+//console.log(usr.firstName,usr.lastName,usr.age,usr.info.adress,usr.info.city,usr.info.country);//
+//console.log(usr)
---
+++ dfghjy/Task.js
@@ -0,0 +1,127 @@
+
+///////////////////////////////////////////////////////////////////////////// Tapşırıq 1
+
+// let number = 9;
+
+//if (number > 0) {
+   // console.log("The number is positive.");
+//} else if (number < 0) {
+    //console.log("The number is negative.");
+//} else {
+    //console.log("The number is zero.");
+//
+
+
+///////////////////////////////////////////////////////////////////////////// Tapşırıq 2
+
+//let number = 17;
+
+//if (number % 2 === 0) {
+    //console.log(number + " pair");
+//} else {
+    //console.log(number + " single");
+//}
+
+///////////////////////////////////////////////////////////////////////////// Tapşırıq 3
+
+//let num1 = 15;
+//let num2 = 23;
+
+//if (num1 > num2) {
+   // console.log(num1 + " greater " + num2);
+//} else if (num2 > num1) 
+    //console.log(num2 + " greater " + num1);
+
+///////////////////////////////////////////////////////////////////////////// Tapşırıq 4
+
+//var reqemTap;
+//reqemTap=75 //reqem daxil et
+
+//if (reqemTap >=90) {
+    //console.log( "A");
+//}else if (reqemTap >=80){
+    //console.log( "B")
+//else if (reqemTap >=70){
+    //console.log( "C")
+//}else if (reqemTap >=60){
+    //console.log( "D")
+//}else if (reqemTap >=50){
+    //console.log( "E")
+//}
+
+///////////////////////////////////////////////////////////////////////////// Tapşırıq 5
+
+///////////////////////////////////////////////////////////////////////////// Tapşırıq 6
+
+///////////////////////////////////////////////////////////////////////////// Tapşırıq 7
+
+var reQem
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////Task1///////////////////////////////////////////////////////////////
+//1.
+// let result = 12 * 8;
+// console.log(result);
+
+///////////////////////////////////////2
+//let x = 0; 
+//x += 10 / 2;
+//console.log(x);
+
+///////////////////////////////////////3
+// var num1 = 20;
+// var num2 = 17;
+// var sum = num1 + num2;
+// console.log("num1 and num2 is: " + sum);
+///////////////////////////////////////4
+
+// var name = 'Isa';
+//var surname = 'Aliyev';
+// var birthOfYear = 2005;
+// var info = name + ' ' + surname + ' ' + birthOfYear;
+// console.log(info);
+
+///////////////////////////////////////5
+
+
+
+///////////////////////////////////////6
+// var cityName = "Baku";
+// console.log(cityName);
+// cityName = "Gence";
+// console.log(cityName);
+///////////////////////////////////////////////////////////////////////////////////////////////Task2
+//let a = 1, b = 1;
+//let c = ++a;
+//let d = b++;
+//console.log(c);  =2
+//console.log(d);  =1
+//console.log(b);  =2
+///////////////////////////////////////////////////////////////////////////////////////////////Task3
+//let a = 2;
+//let x = 1 + (a *= 2)
+//console.log(x); =5
+///////////////////////////////////////////////////////////////////////////////////////////////Task4
+// console.log('test1', test); // ?
+// {
+//     let test = "something"
+//     console.log('test2', test); // ?             /test1 undefined     test2 something         test3undefined
+// }
+// console.log('test3', test);  // ?
+
+
+// console.log('test1', test);  // ?
+// {
+//     var test = "something"
+//     console.log('test2', test); // ?              /test1 undefined test2 something       test3something
+// }
+// console.log('test3', test); // ?
+
+
