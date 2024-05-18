2a4800ee40241b9f196ce345aa066dddaaf29f56
isa aliyev <isaaliyevv888@gmail.com>
2024-05-18 12:44:45 +0400

    feat:1

---
+++ 01.Introduction/script.js
@@ -0,0 +1,174 @@
+//var-eynni dəyişən istifadə olunur.
+//let-fərqli fərqli dəyişən istifadə olunur.---dəyişmə ehtimal olunursa let lə yazılır.
+//const-dəyişməyən constat dəyərlər
+
+//var-keyword
+//user-variable
+//'Isa'-value
+//////////////////////////////////////////////////////////////////////////////////
+//1.camelCase
+//2.PascaCase
+//3.snake_case
+//4.kebab-case
+
+//1. let firstName ='Isa'
+//console.log(firstName)
+//2. let FirstName ='Isa'
+//console.log(FirstName)
+//3. let first_name ='Isa'
+//console.log(first_name)
+//4. let firstName ='Isa'
+//console.log(firstName)
+//console.log(firstName)
+
+//let firstName='Isa'
+//let lastName="Aliyev"
+//let fullName= `${firstName} ${lastName}`
+//console.log(fullName)
+//////////////////////////////////////////////////////////////////////////////////
+//var ad = 'isa'
+//var soyad = 'aliyev'
+//var ata = 'islam'
+//console.log(ad,soyad,ata)
+//////////////////////////////////////////////////////////////////////////////////
+//*_vurma
+//+_toplama
+//-_çıxma
+//**_üstü
+// /_bölür
+// %_faiz
+// ''-string
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+//if-başda verilir şərtdir
+//else-
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////// Tapşırıq 1
+//function-bir kodu dəfələrcə istifadə edəcəyimiz zaman istifadə edilir.
+
+// let number = 9;
+
+//if (number > 0) {
+   // console.log("The number is positive.");
+//} else if (number < 0) {
+    //console.log("The number is negative.");
+//} else {
+    //console.log("The number is zero.");
+//}
+
+///////////////////////////////////////////////////////////////////////////// Tapşırıq 2
+//let number = 17;
+
+//if (number % 2 === 0) {
+    //console.log(number + " pair");
+//} else {
+    //console.log(number + " single");
+//}
+
+///////////////////////////////////////////////////////////////////////////// Tapşırıq 3
+
+//let num1 = 15;
+//let num2 = 23;
+
+//if (num1 > num2) {
+   // console.log(num1 + " greater " + num2);
+//} else if (num2 > num1) 
+    //console.log(num2 + " greater " + num1);
+
+///////////////////////////////////////////////////////////////////////////// Tapşırıq 4
+///////////////////////////////////////////////////////////////////////////// Tapşırıq 5
+// function calculatorTickets(age){
+    //  if (age <6 ) {
+        //  return 0;
+    //  }else if (age>=7 && age<=18){
+        //   return 5;
+    //    } else {
+        //    return 10;
+    //    }
+//    }
+//    let age = 25;
+//  console.log( calculatorTickets(2));
+///////////////////////////////////////////////////////////////////////////// Tapşırıq 6
+
+///////////////////////////////////////////////////////////////////////////// Tapşırıq 7
+
+// function gamePlay(number){
+    // if (number = 13){
+        // return 'Congratulations are true';
+    // }
+    // else if (number = 1);{
+        // return 'The answer is false';
+    // }
+// }
+// console.log(gamePlay(12));
+
+
+
+//////////////////////////////////////////////////////////////////Task1///////////////////////////////////////////////////////////////
+//1.
+// let result = 12 * 8;
+// console.log(result);
+
+///////////////////////////////////////2
+//let x = 0; 
+//x += 10 / 2;
+
+//console.log(x);
+///////////////////////////////////////3
+// var num1 = 20;
+// var num2 = 17;
+// var sum = num1 + num2;
+// console.log("num1 and num2 is: " + sum);
+///////////////////////////////////////4
+// var name = 'Isa';
+//var surname = 'Aliyev';
+// var birthOfYear = 2005;
+// var info = name + ' ' + surname + ' ' + birthOfYear;
+// console.log(info);
+///////////////////////////////////////5
+//let remainder = 17 % 12;
+//console.log("The remainder when 17 is divided by 12 is: " + remainder);
+///////////////////////////////////////6
+//var cityName = "Baku";
+//console.log(cityName);
+//cityName = "Gence";
+//console.log(cityName);
+///////////////////////////////////////////////////////////////////////////////////////////////Task2
+//let a = 1, b = 1;
+//let c = ++a;
+//let d = b++;
+//console.log(c);=2
+//console.log(d);=1
+//console.log(b);=2
+///////////////////////////////////////////////////////////////////////////////////////////////Task3
+//let a = 2;
+//let x = 1 + (a *= 2)
+//console.log(x);=5
+///////////////////////////////////////////////////////////////////////////////////////////////Task4
+// console.log('test1', test); // ?
+// {
+//     let test = "something"
+//     console.log('test2', test); // ?             /test1 undefined     test2 something         test3undefined
+// }
+// console.log('test3', test);  // ?
+
+
+// console.log('test1', test);  // ?
+// {
+//     var test = "something"
+//     console.log('test2', test); // ?              /test1 undefined test2 something       test3something
+// }
+// console.log('test3', test); // ?
+
+
